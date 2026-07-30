b9d2146f48f2f93317fbef958ddae06fbe08a91f
Bandita Bharadwaj <banditabharadwaj.2003@gmail.com>
2026-07-30 15:34:44 +0530

    Update frontend API URL to Render backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_BASE = "http://127.0.0.1:5000";
+const API_BASE = "https://malaria-object-detection.onrender.com";
 
 function App() {
 
